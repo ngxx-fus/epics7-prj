06b872886b9b37ddf5116e05aed8c89775f76886
ngxx-fus <msnp@outlook.com.vn>
2025-03-23 06:41:43 +0700

    changed

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,7 @@ void setup(){
 }
 
 void loop(){
-    uint8_t c_max = 1;
+    uint8_t c_max = 2;
     while(0x1){
         delay(50);
         float d =  ultrasonic_sensor_0.measure();
@@ -13,10 +13,12 @@ void loop(){
         d_sum = (c < 2)?d:(d_sum+d);
         d_delta = (c < 2)?0:(d_delta+abs((d_sum/c)-d));
         if(c<c_max) continue;
-        logger.log(
-            "Count: ", c, 
-            "\t\td_avg: ", d_sum/c, " cm"
-            "\t\td_delta: ", (d_delta*100/d_sum), " cm"
-        );
+        #if defined(LOGGER) && LOG == true
+            LOGGER(
+                "Count: ", c, 
+                "\t\td_avg: ", d_sum/c, " cm"
+                "\t\td_delta: ", (d_delta*100/d_sum), " cm"
+            );
+        #endif
     }
 }
